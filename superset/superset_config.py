9bb7b26ede47456ad077a58ba468e949ccd22507
Medaziz-Effios-expertise <aladhari@effios-expertise.com>
2023-05-05 12:44:13 +0100

    modified:   superset/superset_config.py

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
-from flask_appbuilder.security.manager import AUTH_OID
-from superset.Keycloak.security_manager import OIDCSecurityManager
+#from flask_appbuilder.security.manager import AUTH_OID
+#from superset.Keycloak.security_manager import OIDCSecurityManager
 from superset.views.custom_views import LegalView, RGPDView
 
 SECRET_KEY = 'Ak+NMzarpmGp8rpwQaeJtUM5TER7x6pyfSiDtJ8nzh5t1G+0tJryBa4d'
@@ -11,6 +11,7 @@ APP_NAME = "EFFIOS_SUPERSET"
 APP_ICON = "/static/assets/images/Ministry_logo.png"
 LOGO_TARGET_PATH = '/'
 FAVICONS = [{"href": "/static/assets/images/Ministry_logo.ico"}]
+""""
 AUTH_TYPE = AUTH_OID
 OIDC_CLIENT_SECRETS = r'/home/main_superset/superset/superset/Keycloak/keycloak_settings.json'
 OIDC_ID_TOKEN_COOKIE_SECURE = False
@@ -18,7 +19,7 @@ OIDC_REQUIRE_VERIFIED_EMAIL = False
 CUSTOM_SECURITY_MANAGER = OIDCSecurityManager
 AUTH_USER_REGISTRATION = True
 AUTH_USER_REGISTRATION_ROLE = 'Public'
-
+"""
 LANGUAGES = {
     'en': {'flag': 'us', 'name': 'English'},
     'fr': {'flag': 'fr', 'name': 'Français'}
