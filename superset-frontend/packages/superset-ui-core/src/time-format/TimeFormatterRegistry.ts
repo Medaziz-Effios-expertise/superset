7bea51bad9b0be884ba11ebe215e5279ffc66eb8
Medaziz-Effios-expertise <aladhari@effios-expertise.com>
2023-06-20 12:12:10 +0100

    fixed some front files build issues

--- superset-frontend/packages/superset-ui-core/src/time-format/TimeFormatterRegistry.ts
+++ superset-frontend/packages/superset-ui-core/src/time-format/TimeFormatterRegistry.ts
@@ -27,10 +27,10 @@ const FR_D3_TIME_FORMAT: TimeLocaleDefinition = {
   date: '%-m/%-d/%Y',
   time: '%-I:%M:%S %p',
   periods: ['AM', 'PM'],
-  days: ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi',],
+  days: ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'],
   shortDays: ['dim.', 'lun.', 'mar.', 'mer.', 'jeu.', 'ven.', 'sam.'],
-  months: ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre',],
-  shortMonths: ['janv.', 'févr.', 'mars', 'avr.', 'mai', 'juin', 'juil.', 'août', 'sept.', 'oct.', 'nov.', 'déc.',],
+  months: ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'],
+  shortMonths: ['janv.', 'févr.', 'mars', 'avr.', 'mai', 'juin', 'juil.', 'août', 'sept.', 'oct.', 'nov.', 'déc.'],
 };
 export default class TimeFormatterRegistry extends RegistryWithDefaultKey<
   TimeFormatter,
