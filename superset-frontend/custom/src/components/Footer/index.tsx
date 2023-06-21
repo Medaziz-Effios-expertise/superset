b697fd411d76ec4385f22b87dd79b4c4ff756aa4
Medaziz-Effios-expertise <aladhari@effios-expertise.com>
2023-06-21 15:51:56 +0100

    modified:   superset-frontend/custom/src/components/Footer/index.tsx => updated the footer component 	modified:   superset/static/custom/legal.html => few changes on the page 	modified:   superset/static/custom/rgpd.html  => few changes on the page

--- superset-frontend/custom/src/components/Footer/index.tsx
+++ superset-frontend/custom/src/components/Footer/index.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+import { t } from '@superset-ui/core';
+import React from 'react';
 
 const Footer = () => {
+  const M_legales = t('Mentions Légales');
+  const M_rgpd = t('Mentions RGPD');
+
+  const footerStyle: React.CSSProperties = {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: '40px',
+    backgroundColor: '#FCFCFC',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '0 20px',
+  };
+
+  const linkStyle: React.CSSProperties = {
+    margin: '0 10px',
+    color: '#666',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+    fontSize: '18px',
+  };
+
   return (
-    <footer style={{ position: 'absolute', bottom: 0, width: '100%', height: '40px', backgroundColor: '#FCFCFC', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0 20px' }}>
+    <footer style={footerStyle}>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <a href="/static/custom/legal.html" style={{ margin: '0 10px', color: '#666', textDecoration: 'none', fontWeight: 'bold', fontSize: '18px' }}>mentions légales</a>
-          <a href="/static/custom/rgpd.html" style={{ margin: '0 10px', color: '#666', textDecoration: 'none', fontWeight: 'bold', fontSize: '18px' }}>mentions RGPD</a>
+          <a href="/static/custom/legal.html" style={linkStyle}>
+            {M_legales}
+          </a>
+          <a href="/static/custom/rgpd.html" style={linkStyle}>
+            {M_rgpd}
+          </a>
         </div>
       </div>
     </footer>
-
   );
 };
 
